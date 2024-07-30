093cfb3f4099096b29c0de9ceafd6354eb70eca2
Tami-Ann <tamifromit@outlook.com>
2024-07-30 06:31:00 -0500

    Created hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+    }
+  }
+});
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
