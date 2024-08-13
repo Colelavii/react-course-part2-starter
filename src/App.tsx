e15e8be577704ba070018f343c3283d7b64be47a
Tami-Ann <tamifromit@outlook.com>
2024-08-12 22:43:53 -0500

    Showing mutation progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import PostList from "./react-query/PostList";
 import PostListPagi from "./react-query/PostListPagi";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <PostListPagi />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
--- src/react-query/hooks/usePostsPagi.tsx
+++ src/react-query/hooks/usePostsPagi.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -13,23 +13,37 @@ interface PostQuery {
   pageSize: number;
 }
 
-const usePostsPagi = (query: PostQuery) => {
-  const fetchPosts = () =>
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts", {
-        params: {
-          _start: (query.page - 1) * query.pageSize,
-          _limit: query.pageSize,
-        },
-      })
-      .then((res) => res.data);
+interface InfinitePostQuery {
+  pageSize: number;
+}
 
-  return useQuery<Post[], Error>({
+const usePostsPagi = (query: InfinitePostQuery) =>
+  // const fetchPosts = () =>
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts", {
+  //       params: {
+  //         _start: (query.page - 1) * query.pageSize,
+  //         _limit: query.pageSize,
+  //       },
+  //     })
+  //     .then((res) => res.data);
+
+  // return useQuery<Post[], Error>({
+  useInfiniteQuery<Post[], Error>({
     queryKey: ["posts", query], // toplevel object, query - any time the query changes react query will fetch the posts from the backend
-    queryFn: fetchPosts,
+    queryFn: ({ pageParam = 1 }) =>
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (pageParam - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
     staleTime: 1 * 60 * 1000, //1min
     keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
-};
-
 export default usePostsPagi;
